9125980395d2def650e4b9cf83532cd3ee9838ed
keokistevenson <keokistevenson@gmail.com>
2026-05-19 19:40:13 -0400

    Create code for DB connection.

---
+++ db/connection.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DATABASE_URI);
+
+const db = mongoose.connection;
+db.on("error", (err)=> console.log(err.message + " is MongoDB is not running?"));
+db.on("connected", ()=> console.log("MongoDB is connected"));
+db.on("disconnected", () => console.log("MongoDB is disconnected."));
+
+
