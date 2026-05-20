e42b9c4b53751ae8290deaa6abebf909181d4fae
keokistevenson <keokistevenson@gmail.com>
2026-05-19 23:37:10 -0400

    Fixed a copy paste environment variable error.

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.DATABASE_URI);
+mongoose.connect(process.env.DB_URI);
 
 const db = mongoose.connection;
 db.on("error", (err)=> console.log(err.message + " is MongoDB is not running?"));
